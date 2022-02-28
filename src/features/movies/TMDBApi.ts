209ba50e9ce5ace987c5575ef02a7270dbaefb8a
Roman Polyakov <xreyaf@gmail.com>
2022-02-28 11:49:39 +0300

    fetchBaseQuery replaced with axiosBaseQuery

--- src/features/movies/TMDBApi.ts
+++ src/features/movies/TMDBApi.ts
@@ -1,18 +1,40 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, BaseQueryFn } from "@reduxjs/toolkit/query/react";
+import axios, { AxiosRequestConfig, AxiosError } from "axios";
 import { IDetails, IMedia, IResponse } from "./types";
 
 const API_KEY = "b50461522e6b25f122838ff594a78e50";
 
+const axiosBaseQuery =
+  (
+    { baseUrl }: { baseUrl: string } = { baseUrl: "" }
+  ): BaseQueryFn<{
+    url: string
+    method: AxiosRequestConfig["method"]
+    params: AxiosRequestConfig["params"]
+    data?: AxiosRequestConfig["data"]
+  }, unknown, unknown, unknown> =>
+    async ({ url, method, params, data }) => {
+      try {
+        const result = await axios({ url: baseUrl + url, method, params, data });
+        return { data: result.data };
+      } catch (axiosError) {
+        let err = axiosError as AxiosError;
+        return {
+          error: { status: err.response?.status, data: err.response?.data }
+        };
+      }
+    };
 
 const TMDBApi = createApi({
   reducerPath: "api/movies",
-  baseQuery: fetchBaseQuery({
+  baseQuery: axiosBaseQuery({
     baseUrl: "https://api.themoviedb.org/3"
   }),
   endpoints: builder => ({
     getTrending: builder.query<IMedia[], unknown>({
       query: ({ media_type, time }) => ({
         url: `/trending/${media_type}/${time}`,
+        method: "get",
         params: {
           api_key: API_KEY,
           language: "en_US"
@@ -23,6 +45,7 @@ const TMDBApi = createApi({
     getPopular: builder.query<IMedia[], string | void>({
       query: (media_type) => ({
         url: `/${media_type}/popular`,
+        method: "get",
         params: {
           api_key: API_KEY,
           language: "en_US"
@@ -33,6 +56,7 @@ const TMDBApi = createApi({
     searchMedia: builder.query<IMedia[], string | void>({
       query: (searchTitle) => ({
         url: `/search/multi`,
+        method: "get",
         params: {
           api_key: API_KEY,
           language: "en_US",
@@ -44,6 +68,7 @@ const TMDBApi = createApi({
     getDetails: builder.query<IDetails, unknown>({
       query: ({ mediaType, id }) => ({
         url: `/${mediaType}/${id}`,
+        method: "get",
         params: {
           api_key: API_KEY,
           language: "en_US"
